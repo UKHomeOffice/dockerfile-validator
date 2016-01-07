1b917db2291c8c043d68a3091ab0006dc46bcfd8
Ivan Pedrazas <ipedrazas@gmail.com>
2016-01-07 16:37:03 +0000

    refactor main

--- main.go
+++ main.go
@@ -1,68 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"github.com/docker/docker/builder/dockerfile/command"
-	"github.com/docker/docker/builder/dockerfile/parser"
 	"gopkg.in/yaml.v2"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
-	"strings"
 )
 
 var debug = false
-
-// DockerfileFromPath reads a Dockerfiler from a oath
-func DockerfileFromPath(input string) (*Dockerfile, error) {
-	payload, err := ioutil.ReadFile(input)
-	if err != nil {
-		return nil, err
-	}
-	if debug {
-		log.Println(string(payload))
-	}
-	return DockerfileRead(bytes.NewReader(payload))
-}
-
-// DockerfileRead reads a Dockerfile as io.Reader
-func DockerfileRead(input io.Reader) (*Dockerfile, error) {
-	dockerfile := Dockerfile{}
-
-	root, err := parser.Parse(input)
-	if err != nil {
-		return nil, err
-	}
-	dockerfile.root = root
-
-	return &dockerfile, nil
-}
-
-// GetFrom returns the current FROM
-func (d *Dockerfile) From() string {
-	for _, node := range d.root.Children {
-		if node.Value == command.From {
-			from := strings.Split(node.Original, " ")[1]
-			if debug {
-				log.Println(from)
-			}
-			return from
-		}
-	}
-
-	return ""
-}
-
-// String returns a docker-readable Dockerfile
-func (d *Dockerfile) String() string {
-	lines := []string{}
-	for _, child := range d.root.Children {
-		lines = append(lines, child.Original)
-	}
-	return strings.Join(lines, "\n")
-}
 
 // load rules file
 func loadRules(ruleFile string) (Rules, error) {
