fa3243cd673f891c3619491355f658cf685ffb35
Ivan Pedrazas <ipedrazas@gmail.com>
2016-01-13 16:31:29 +0000

    dockerfile validation enabled

--- validate-dockerfile.sh
+++ validate-dockerfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-R=$(curl --write-out %{http_code} --silent --output /dev/null -F dockerfile=@Dockerfile.fail_unittest localhost:8080/validate  )
+R=$(curl --write-out %{http_code} --silent --output /dev/null -F dockerfile=@Dockerfile http://dockerfile-validator.ipedrazas.k8s.co.uk/validate  )
 
 echo $R
 
