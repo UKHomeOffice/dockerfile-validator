542bf6c9a7cc2a7d7b34dbd2f2e53bf74a876538
Ivan Pedrazas <ipedrazas@gmail.com>
2016-01-18 14:10:03 +0000

    command utility added

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 	"log"
 	"net/http"
 	"os"
@@ -61,14 +63,40 @@ func (v Validation) validate() (bool, string) {
 
 }
 
-func main() {
+var (
+	startDaemon = kingpin.Flag("daemon", "start a simple HTTP serving your slides.").Short('d').Bool()
+	port        = kingpin.Flag("port", "port where to run the server.").Short('p').Default("8080").Int()
 
-	debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
-	rules, _ = loadRulesFromFile("rules.yaml")
-	log.Println("listening in port 8080")
-	http.HandleFunc("/validate", validateHandler)
-	http.HandleFunc("/setup", uploadHandler)
-	http.HandleFunc("/rules", uploadRulesHandler)
-	http.HandleFunc("/", defaultHandler)
-	http.ListenAndServe(":8080", nil)
+	dockerfile = kingpin.Flag("dockerfile", "dockerfile to be analysed").Short('f').Default("Dockerfile").String()
+	rulesFile  = kingpin.Flag("rules", "Rules file").Short('r').String()
+)
+
+func main() {
+	kingpin.Parse()
+
+	if *startDaemon {
+		debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+		rules, _ = loadRulesFromFile("rules.yaml")
+		log.Println("Daemon is listening in port", strconv.Itoa(*port))
+		http.HandleFunc("/validate", validateHandler)
+		http.HandleFunc("/setup", uploadHandler)
+		http.HandleFunc("/rules", uploadRulesHandler)
+		http.HandleFunc("/", defaultHandler)
+		http.ListenAndServe(":8080", nil)
+
+	} else {
+		rules, _ = loadRulesFromFile(*rulesFile)
+		dfile, _ := DockerfileFromPath(*dockerfile)
+
+		v := Validation{rules, dfile}
+		valid, msg := v.validate()
+		if valid {
+			fmt.Println("Dockerfile valid")
+			os.Exit(0)
+		} else {
+			fmt.Println("Docker file not vallid:", msg)
+			os.Exit(1)
+		}
+
+	}
 }
